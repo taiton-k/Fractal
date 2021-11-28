c48b2bea0cdba1de247af2018ef3237057c6e38d
taiton-k <taitanknowcat@cock.li>
2021-11-28 10:02:08 +0900

    Rename Program.hpp and Program.cpp to ShaderProgram.hpp and ShaderProgram.cpp.

--- Program.cpp
+++
@@ -1,8 +0,0 @@
-#include "Program.hpp"
-
-Program::Program():programObject(glCreateProgram()){
-        if(programObject==0){
-                std::cerr << "glCreateProgram() Failed." << std::endl;
-                std::exit(-1);
-        }
-}
